d8cf9f70d6199729d12a97acf735dbe6321c7562
Tzuwiing <jusuf1909@student.abudzar.sch.id>
2025-10-02 09:41:11 +0700

    d

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/login_page/login.dart';
-import 'package:flutter_project/login_page/profile.dart';
 import 'package:page_transition/page_transition.dart';
 
 void main() {
